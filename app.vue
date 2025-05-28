20a2b2119f995f6d167819ec1ee946944d6f3a37
tOCHSKa <144660217+tOCHSKa@users.noreply.github.com>
2025-05-28 10:36:05 +0200

    feat: add google Adsense script in app.vue

--- app.vue
+++ app.vue
@@ -32,6 +32,11 @@ useHead({
         gtag('js', new Date());
         gtag('config', 'G-8K600SK026');
       `
+    },
+    {
+      async: true,
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4373658027660331',
+      crossorigin: 'anonymous'
     }
   ]
 })
